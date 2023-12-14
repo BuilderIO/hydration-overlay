0b8ac3c598b8d7e477bc8f5694033c07230809e1
Sami Jaber <jabersami@gmail.com>
2023-12-14 11:46:04 -0400

    bring back example

--- tests/next-pages/src/pages/index.tsx
+++ tests/next-pages/src/pages/index.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <>
       <div>normal content</div>
-      {/* {typeof window === "undefined" ? (
+      {typeof window === "undefined" ? (
         <div>SERVER RENDERED DIV</div>
       ) : (
         <span>BROWSER RENDERED SPAN</span>
-      )} */}
+      )}
       <div>
         <div>Hello world!</div>
         <button>CLOSE</button>
