ee99699fc2a94c6ff2164419cd79ee2bd6ee8125
mlmmn <8261859+mlmmn@users.noreply.github.com>
2024-01-22 18:41:32 +0100

    fix: remove event filename condition from initializer (#38)
    Fixes https://github.com/BuilderIO/hydration-overlay/issues/36
    
    ---------
    
    Co-authored-by: Miłosław Politowski <miloslaw.politowski@olx.pl>
    Co-authored-by: Sami Jaber <me@sami.website>

---
+++ .changeset/stale-phones-relate.md
@@ -0,0 +1,5 @@
+---
+"@builder.io/react-hydration-overlay": patch
+---
+
+Fix: remove filename checks (improves hydration error check)
--- packages/lib/src/hydration-overlay-initializer.js
+++ packages/lib/src/hydration-overlay-initializer.js
@@ -2,10 +2,9 @@ window.BUILDER_HYDRATION_OVERLAY = {};
 
 window.addEventListener("error", (event) => {
   const msg = event.message.toLowerCase();
-  const isReactDomError = event.filename.includes("react-dom");
   const isHydrationMsg = msg.includes("hydration") || msg.includes("hydrating");
 
-  if (isReactDomError && isHydrationMsg) {
+  if (isHydrationMsg) {
     window.BUILDER_HYDRATION_OVERLAY.ERROR = true;
     let appRootEl = document.querySelector(
       window.BUILDER_HYDRATION_OVERLAY.APP_ROOT_SELECTOR
